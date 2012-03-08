141a2a9a8b6bff32e4b052cb5abb056c0b670f9e
Peter Downs <peter.l.downs@gmail.com>
2012-03-07 21:14:06 -0500

    Passes in sep as an argument

--- do_test.py
+++ do_test.py
@@ -8,25 +8,26 @@ print "Cleaning sourcefile (prideandprejudice.txt)"
 dq = re.sub("[^a-zA-Z0-9_ ]", "", dq.strip().lower()) # lowercase all the characters.
 
 sep = '\0'
+
 # uncomment the next two lines to analyze words
 sep = ' '
-dq = dq.split(sep) # uncomment to analyze words, not characters
+dq = dq.split(sep)
 
 # do the text generation. this takes a while.
 from ngram import stochastic_walk, calc_up_to_ngram
 n_grams = calc_up_to_ngram(dq, 5)
 
 print "Walking (1-deep) ... "
-a = stochastic_walk(dq, 50, 1, n_grams) 
+a = stochastic_walk(dq, 50, 1, n_grams, sep) 
 print "\t%s" % sep.join(a)
 print ""
 
 print "Walking (3-deep) ... "
-b = stochastic_walk(dq, 50, 3, n_grams)
+b = stochastic_walk(dq, 50, 3, n_grams, sep)
 print "\t%s" % sep.join(b)
 print ""
 
 print "Walking (5-deep) ... "
-c = stochastic_walk(dq, 50, 5, n_grams)
+c = stochastic_walk(dq, 50, 5, n_grams, sep)
 print "\t%s" % sep.join(c)
 print ""
